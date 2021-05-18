294c15a1b56fa58b59930707401ae73b2862ec31
Andrew <andrewlee1807@gmail.com>
2021-05-18 18:15:52 +0900

    add token

--- Pytorch/nlp/bert.py
+++ Pytorch/nlp/bert.py
@@ -1,14 +1,19 @@
-import numpy as np
-a = np.random.randint(1,20, 10)
-print(a)
-for i in range(0, len(a)-1):
-    min = a[i]
-    index = i
-    for j in range(i+1, len(a)):
-        if a[j] < min:
-            min = a[j]
-            index = j
-    a[i], a[index] = a[index], a[i]
-print(a)
+import torch
+from transformers import BertTokenizer, BertModel
+import matplotlib.pyplot as plt
 
+# Load pre-trained model tokenizer (vocabulary)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
+print(tokenizer)
+
+text = "Here is the sentence I want embeddings for."
+marked_text = "[CLS] " + text + " [SEP]"
+
+# Tokenize our sentence with the BERT tokenizer.
+tokenized_text = tokenizer.tokenize(marked_text)
+
+# Print out the tokens.
+print (tokenized_text)
+
+print(type(tokenized_text))
